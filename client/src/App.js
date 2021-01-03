83e08cd7093ac5a8b136efe63bbb1aa922083922
Cierra Higgins <cierramhiggins@gmail.com>
2021-01-02 16:55:17 -0800

    0102: Added Hooks, Styled components, removed extra components

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
-import React, { Component } from 'react';
-import './portfolio.scss';
+import React, { Component, useState } from 'react';
+// components
 import About from './About.js';
 import Experience from './Experience.js';
-import Header from './Header.js';
-import SideBar from './SideBar.js';
-import Footer from './Footer.js';
 import Welcome from './Welcome.js';
+// images and scss
 import styled from 'styled-components'
+import email from './images/gmail_35.png';
+import github_logo from './images/github_35.png';
+import linkedin_logo from './images/linkedin_35.png';
+import './portfolio.scss';
 
 class App extends Component {
 
@@ -16,7 +18,7 @@ class App extends Component {
       data: null,
       showFooter: false,
       opacity: '0',
-      apiResponse: 'YEY',
+      apiResponse: '',
       hideChev: false
     };
   }
@@ -36,10 +38,8 @@ class App extends Component {
       window.onscroll = () => {
         let currentScrollPos = window.pageYOffset;
         let maxScroll = document.body.scrollHeight - window.innerHeight;
-        // console.log(maxScroll)
         if (currentScrollPos < 200 && currentScrollPos < maxScroll) {
           this.setState({ opacity: '0', hideChev: 'block' })
-          // console.log(currentScrollPos)
         } else {
           this.setState({ opacity: '1', hideChev: 'none' })
         }
@@ -76,8 +76,8 @@ class App extends Component {
         position: absolute;
         width: 24px;
         height: 24px;
-        border-left: 1px solid #fff;
-        border-bottom: 1px solid #fff;
+        border-left: 3px solid #fff;
+        border-bottom: 3px solid #fff;
         -webkit-transform: rotate(-45deg);
         transform: rotate(-45deg);
         -webkit-animation: sdb04 2s infinite;
@@ -115,9 +115,11 @@ class App extends Component {
                 <div className="about">
                     <SideBar/>
                     <Welcome/>
-                    <a href="#about" style={{ display: `${this.state.hideChev}`}}>
-                        <ScrollDown/>
-                    </a>
+                    <div className="scrollDown">
+                        <a href="#about" style={{ display: `${this.state.hideChev}`}}>
+                            <ScrollDown/>
+                        </a>
+                    </div>
                     <div className="hide" style={{ opacity: `${this.state.opacity}`}}>
                         <About/>
                         <StyledButton download="resume" href="resume.pdf" target="_blank">DOWNLOAD RESUME</StyledButton>
@@ -129,14 +131,67 @@ class App extends Component {
                         <StyledButton href="mailto:cierramhiggins@gmail.com">CONTACT ME</StyledButton>
                     </div>
                 </div>
-                <div className="footer">
-                    <Footer/>
-                </div>
-                <div className="footerText">Designed and built by Cierra Higgins</div>
+                <Footer/>
             </div>
         </div>
     );
   }
 }
 
+function Footer() {
+    return (
+        <div>
+            <div className="footer">
+                <a href="mailto:cierramhiggins@gmail.com"><img src={email} className="icon" alt="logo"/></a>
+                <a href='https://github.com/chigginss'><img src={github_logo} className="icon" alt="logo"/></a>
+                <a href='https://www.linkedin.com/in/cierra-m-higgins/'><img src={linkedin_logo} className="icon" alt="logo" /></a>
+            </div>
+            <div className="footerText">Designed and built by Cierra Higgins</div>
+        </div>
+    );
+}
+
+function Header() {
+    const [open, setOpen] = useState(false);
+  
+    return (
+        <div className="headerBar">
+            <div className="headerContainer">
+                <div className="headerName">CIERRA HIGGINS</div>
+            </div>
+            <div className="headerLinks">
+                <a href="#about" className="headerLink">About</a>
+                <a href="#experience" className="headerLink">Experience</a>
+                <a href="#projects" className="headerLink">Projects</a>
+                <a href="#contact" className="headerLink">Contact</a>
+            </div>
+            <div className={open ? 'burgerOpen' : 'burgerClosed'} onClick={() => setOpen(!open)}>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+            <div className={open ? 'menuOpen' : 'menuClosed'}>
+                <a href="#about" onClick={() => setOpen(!open)}>About</a>
+                <a href="#experience" onClick={() => setOpen(!open)}>Experience</a>
+                <a href="#projects" onClick={() => setOpen(!open)}>Projects</a>
+                <a href="#contact" onClick={() => setOpen(!open)}>Contact</a>
+            </div>
+        </div>
+    );
+}
+
+function SideBar() {
+    return (
+        <div className="headerContent">
+          <div className='header'>
+            <div className="contactInfo">
+              <a href="mailto:cierramhiggins@gmail.com"><img src={email} className="icon" alt="logo"/></a>
+              <a href='https://github.com/chigginss'><img src={github_logo} className="icon" alt="logo"/></a>
+              <a href='https://www.linkedin.com/in/cierra-m-higgins/'><img src={linkedin_logo} className="icon" alt="logo" /></a>
+            </div>
+          </div>
+        </div>
+    );
+}
+  
 export default App;
