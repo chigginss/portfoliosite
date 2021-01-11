66e98a9bcb51f428f8e158e607e8ce5a82ba57ed
Cierra <cierramhiggins@gmail.com>
2021-01-10 22:40:56 -0800

    Update server.js

--- server.js
+++ server.js
@@ -9,3 +9,12 @@ app.listen(port, () => console.log(`Listening on port ${port}`));
 app.get('/', (req, res) => {
   res.render('index', {});
 });
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+ });
