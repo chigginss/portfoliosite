88656db897ef9d504a3245f57a92cdcec5a04a3e
Cierra <cierramhiggins@gmail.com>
2021-01-13 22:54:20 -0800

    Update server.js

--- server.js
+++ server.js
@@ -3,19 +3,14 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 5000;
 
-// __dirname is the current directory from where the script is running
-app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'build')));
-
-app.get('/ping', function (req, res) {
- return res.send('pong');
-});
-
-// set default engine
-app.set('view engine', 'jade');
-
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'client', 'public', 'index.html'));
-});
-
 app.listen(port, () => console.log(`Listening on port ${port}`));
+
+app.use(express.static(path.join(__dirname, './client/build')))
+
+app.get('*', function(_, res) {
+  res.sendFile(path.join(__dirname, './client/build/index.html'), function(err) {
+    if (err) {
+      res.status(500).send(err)
+    }
+  })
+})
