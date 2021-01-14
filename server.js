e973905a4312c0d8a89fb8666a6d9918f60547c4
Cierra <cierramhiggins@gmail.com>
2021-01-13 23:09:38 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
 const express = require('express');
 const path = require('path');
+const port = process.env.PORT || 8080;
 const app = express();
-const port = process.env.PORT || 5000;
-
-app.listen(port, () => console.log(`Listening on port ${port}`));
-
-app.use(express.static(path.join(__dirname, 'build')))
-
-app.get('*', function(_, res) {
-  res.sendFile(path.join(__dirname, 'build'), function(err) {
-    if (err) {
-      res.status(500).send(err)
-    }
-  })
-})
+// the __dirname is the current directory from where the script is running
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/ping', function (req, res) {
+ return res.send('pong');
+});
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+app.listen(port);
