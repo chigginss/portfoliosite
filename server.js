15a8c40bc4cc958502e61aecc9a8a6a0d6e5f31b
Cierra Higgins <cierramhiggins@gmail.com>
2021-01-13 18:52:38 -0800

    0113: Update NPM scripts, procfile, express routes and server
    - run react and node together with concurrently
    - fix other issues to prepare for deployment
    0113: Update NPM scripts, procfile, express routes and server
    
    - run react and node together with concurrently
    - fix other issues to prepare for deployment
    0113: Update NPM scripts, procfile, express routes and server
    
    - run react and node together with concurrently
    - fix other issues to prepare for deployment
    0113: Update NPM scripts, procfile, express routes and server
    
    - run react and node together with concurrently
    - fix other issues to prepare for deployment
    0113: Update NPM scripts, procfile, express routes and server
    
    - run react and node together with concurrently
    - fix other issues to prepare for deployment
    0113: Update NPM scripts, procfile, express routes and server
    
    - run react and node together with concurrently
    - fix other issues to prepare for deployment
    0113: Update NPM scripts, procfile, express routes and server
    
    - run react and node together with concurrently
    - fix other issues to prepare for deployment
    0113: Update NPM scripts, procfile, express routes and server
    
    - run react and node together with concurrently
    - fix other issues to prepare for deployment
    0113: Update NPM scripts, procfile, express routes and server
    
    - run react and node together with concurrently
    - fix other issues to prepare for deployment
    0113: Update NPM scripts, procfile, express routes and server
    
    - run react and node together with concurrently
    - fix other issues to prepare for deployment

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 const port = process.env.PORT || 5000;
 
-// check server is up and running
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// create a GET route
 app.get('/', (req, res) => {
-  res.render('index', {});
+  res.send({ express: 'Hello From Express' });
 });
+
+//set default engine, and provide [handlebars as] extension
+app.set('view engine', 'jade');
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
