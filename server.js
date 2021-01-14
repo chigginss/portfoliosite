f9a05b2c0d9202a0e1ae53864bf4f3eafb5e0cb6
Cierra <cierramhiggins@gmail.com>
2021-01-13 22:41:16 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,16 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-
+const favicon = require('express-favicon');
+const path = require('path');
+const port = process.env.PORT || 8080;
 const app = express();
-const port = process.env.PORT || 5000;
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.get('/', (req, res) => {
+app.use(favicon(__dirname + '/build/favicon.ico'));
+// the __dirname is the current directory from where the script is running
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/ping', function (req, res) {
+ return res.send('pong');
+});
+app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-
-//set default engine, and provide [handlebars as] extension
-app.set('view engine', 'jade');
-
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.listen(port);
