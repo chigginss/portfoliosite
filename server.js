5d0546216dd86f808403d97db7bafcb8399519e4
Cierra <cierramhiggins@gmail.com>
2021-01-13 22:57:43 -0800

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.listen(port, () => console.log(`Listening on port ${port}`));
 app.use(express.static(path.join(__dirname, './client/build')))
 
 app.get('*', function(_, res) {
-  res.sendFile(path.join(__dirname, './client/build/index.html'), function(err) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'), function(err) {
     if (err) {
       res.status(500).send(err)
     }
