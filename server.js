f25bbd87f6aeea10aa1c4b9a5eb8cd32d0d67119
Cierra Higgins <cierramhiggins@gmail.com>
2021-01-16 23:28:40 -0800

    Update scripts

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const favicon = require('express-favicon');
 const path = require('path');
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 5000;
 const app = express();
 app.use(favicon(__dirname + '/build/client/public/favicon.ico'));
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'public')));
-app.get('/ping', function (req, res) {
- return res.send('pong');
+app.use('/', express.static(path.join(__dirname, "client", "build")))
+
+// Right before your app.listen(), add this:
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "public", "index.html"));
 });
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'client', 'public', 'index.html'));
-});
+
 app.listen(port);
