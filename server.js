e2a06c566888b9473e237a1611a2e58bd6677bc1
Cierra Higgins <cierramhiggins@gmail.com>
2021-01-17 00:09:30 -0800

    Update scripts

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const port = process.env.PORT || 5000;
 const app = express();
 app.use(favicon(__dirname + '/build/client/public/favicon.ico'));
 // the __dirname is the current directory from where the script is running
-app.use(express.static(__dirname));
 app.use('/', express.static(path.join(__dirname, "client", "build")))
 
 // Right before your app.listen(), add this:
