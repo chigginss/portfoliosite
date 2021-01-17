0f588af5a5c44c6f9199b7eb27e704196a54052c
Cierra Higgins <cierramhiggins@gmail.com>
2021-01-17 13:17:36 -0800

    Update Server.js

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const port = process.env.PORT || 5000;
 const app = express();
 app.use(favicon(__dirname + '/client/public/favicon.ico'));
 // the __dirname is the current directory from where the script is running
-app.use('/', express.static(path.join(__dirname, "client")))
+app.use('/', express.static(path.join(__dirname, "build")))
 
 // Right before your app.listen(), add this:
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "public", "index.html"));
+    res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 app.listen(port);
