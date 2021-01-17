d9baae1e6865bac55821d5982ad4fcb6e12d02b1
Cierra Higgins <cierramhiggins@gmail.com>
2021-01-17 00:23:56 -0800

    Remove build

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ const favicon = require('express-favicon');
 const path = require('path');
 const port = process.env.PORT || 5000;
 const app = express();
-app.use(favicon(__dirname + '/build/client/public/favicon.ico'));
+app.use(favicon(__dirname + '/client/public/favicon.ico'));
 // the __dirname is the current directory from where the script is running
-app.use('/', express.static(path.join(__dirname, "client", "build")))
+app.use('/', express.static(path.join(__dirname, "client")))
 
 // Right before your app.listen(), add this:
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "public", "index.html"));
+    res.sendFile(path.join(__dirname, "client", "public", "index.html"));
 });
 
 app.listen(port);
