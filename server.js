4e1d0d4e0315f13924669f11f050c8a29767a28e
Cierra Higgins <cierramhiggins@gmail.com>
2021-01-18 14:09:19 -0800

    Update path

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use('/', express.static(path.join(__dirname, "build")))
 
 // Right before your app.listen(), add this:
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "app", "client", "build", "index.html"));
 });
 
 app.listen(port);
